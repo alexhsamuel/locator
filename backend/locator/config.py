076756856b0ef7b63458301b9a29727111b1486d
Alex Samuel <alex@alexsamuel.net>
2018-06-14 19:51:57 -0400

    Let Flask carry the config.

--- backend/locator/config.py
+++ backend/locator/config.py
@@ -4,51 +4,52 @@ Application configuration.
 
 #-------------------------------------------------------------------------------
 
-class Config:
-
+def get_config():
     # FIXME: Hard-coded for now.  
+    cfg = {
+        "statuses": [
+            "remote",
+            "sick",
+            "medical",
+            "vacation",
+            "personal",
+            "Greenwich",
+            "NYC",
+            "London",
+        ],
 
-    statuses = [
-        "remote",
-        "sick",
-        "medical",
-        "vacation",
-        "personal",
-        "Greenwich",
-        "NYC",
-        "London",
-    ]
-
-    users = {
-        "asamuel", 
-        "ebernste", 
-        "kmalyar",
-        "mlumelsk",
-        "mroiter", 
-        "ptj",
-        "scai", 
-        "slevin",
-        "sgorbey",
-    }
-
-    groups = {
-        "ASD": {
+        "users": {
             "asamuel", 
-            "scai", 
-            "slevin",
-        },
-        "Data": {
             "ebernste", 
             "kmalyar",
-        },
-        "IT": {
-            "mroiter", 
             "mlumelsk",
+            "mroiter", 
+            "ptj",
+            "scai", 
+            "slevin",
+            "sgorbey",
         },
+
+        "groups": {
+            "ASD": {
+                "asamuel", 
+                "scai", 
+                "slevin",
+            },
+            "Data": {
+                "ebernste", 
+                "kmalyar",
+            },
+            "IT": {
+                "mroiter", 
+                "mlumelsk",
+            },
+        }
     }
 
+    # All group members should be users.
+    assert all( u in cfg["users"] for g in cfg["groups"].values() for u in g )
 
+    return cfg
 
-# All group members should be users.
-assert all( u in Config.users for g in Config.groups.values() for u in g )
 
