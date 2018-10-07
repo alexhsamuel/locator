eba9e845ebbb26bc3f94ac24ec8039c68aa7629d
Alex Samuel <alex@alexsamuel.net>
2018-10-07 13:40:13 -0400

    vuekit.

--- vue/src/main.js
+++ vue/src/main.js
@@ -2,6 +2,13 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import Vuikit from 'vuikit'
+import VuikitIcons from '@vuikit/icons'
+
+import '@vuikit/theme'
+
+Vue.use(Vuikit)
+Vue.use(VuikitIcons)
 
 Vue.config.productionTip = false
 
--- vue/src/views/Home.vue
+++ vue/src/views/Home.vue
@@ -1,18 +1,21 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <br>
+    <UserSelect/>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
 import HelloWorld from '@/components/HelloWorld.vue'
+import UserSelect from '@/components/UserSelect.vue'
 
 export default {
   name: 'home',
   components: {
-    HelloWorld
+    HelloWorld,
+    UserSelect,
   }
 }
 </script>
