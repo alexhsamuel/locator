c0e5189f76fff978e99994a297f95349bd55d8f8
Alex Samuel <alex@alexsamuel.net>
2018-11-04 10:32:22 -0500

    Use pug.

--- vue/src/App.vue
+++ vue/src/App.vue
@@ -1,11 +1,11 @@
-<template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
-  </div>
+<template lang="pug">
+#app
+  #nav
+    router-link(to="/") Home
+    |  | 
+    router-link(to="/about") About
+  .uk-container.uk-container-expand.uk-margin-top
+    router-view
 </template>
 
 <style lang="scss">
