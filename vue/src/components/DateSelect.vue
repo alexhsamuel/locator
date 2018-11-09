e23661e71c0888b0cc5d20a5663611fb108adbde
Alex Samuel <alex@alexsamuel.net>
2018-11-09 07:40:40 -0500

    Date picker.

--- vue/src/components/DateSelect.vue
+++ vue/src/components/DateSelect.vue
@@ -1,12 +1,18 @@
 <template lang="pug">
 span.uk-inline
-  input(v-model="date" v-on:input="$emit('input', date)")
+  Datepicker(format="yyyy-MM-dd")
+  // input(v-model="date" v-on:input="$emit('input', date)")
 </template>
 
 <script>
+import Datepicker from 'vuejs-datepicker'
+
 export default {
   name: 'DateSelect',
   props: ['value'],
+  components: {
+    Datepicker,
+  },
 
   data() {
     return {
